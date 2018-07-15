e72a419de264ca023a36632bbba9dbca3d1e2494
Patrick Drechsler <socialcoding@pdrechsler.de>
2018-07-15 20:58:37 +0200

    add service test project

---
+++ DemoApp.ApplicationService.Tests/CustomerServiceTests.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace DemoApp.ApplicationService.Tests
+{
+    public class CustomerServiceTests
+    {
+        [Fact]
+        public void SmokeTest()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
