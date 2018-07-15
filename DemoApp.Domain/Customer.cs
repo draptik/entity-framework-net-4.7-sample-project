86a8767ab372a3359e3a99a63fb0706e40e4cb3a
Patrick Drechsler <socialcoding@pdrechsler.de>
2018-07-15 20:37:15 +0200

    add required Id field to customer

--- DemoApp.Domain/Customer.cs
+++ DemoApp.Domain/Customer.cs
@@ -1,7 +1,10 @@
-﻿namespace DemoApp.Domain
+﻿using System;
+
+namespace DemoApp.Domain
 {
     public class Customer
     {
+        public Guid Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
     }
